be285ff38a6a6aa1ab3b283355527af34c618bc3
nolan <nolancmccleary@gmail.com>
2025-11-24 18:45:37 +0000

    update mouse support

--- init.lua
+++ init.lua
@@ -6,7 +6,7 @@
 -- ========================================
 vim.opt.number = true
 vim.opt.relativenumber = true
-
+vim.opt.mouse = "a"
 
 -- ========================================
 -- Plugins (using packer)
